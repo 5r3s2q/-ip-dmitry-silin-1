5e2c69324c54189af8e78050ffd8a2ec379ae86e
5r3s2q <rebus0410@gmail.com>
2020-08-10 21:16:43 +0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# a = 1.3
+# b = 36
+# c = a + b
+# print(c)
+# print(int(4.7) + 6)
+# x = 'STring'
+# y = 5
+# y = str(y)
+# print(x + str(y))
+# name = input('Enter integer: ')
+# name = int(name)
+name = 2//3
+print(name)
+print(type(name))
